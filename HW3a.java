7d4e6d438a2b6571318a63c9d7fdd21f14c0e8e3
Jon Ginter <jonginter28@gmail.com>
2023-02-23 13:17:30 -0600

    Formatting changes

--- HW3a.java
+++ HW3a.java
@@ -2,32 +2,46 @@ import java.util.*;
 
 public class HW3a {
     public static void main(String[] args) {
+        //make a new scanner to look for user input
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a number between 1 and 10: ");
         int scannedInt = scan.nextInt();
+
+        //Make a switch to output the roman numeral corresponding to the number the user inputs
         String romanNumeral;
         switch (scannedInt) {
-            case 1: romanNumeral = "I";
+            case 1:
+                romanNumeral = "I";
                 break;
-            case 2: romanNumeral = "II";
+            case 2:
+                romanNumeral = "II";
                 break;
-            case 3: romanNumeral = "III";
+            case 3:
+                romanNumeral = "III";
                 break;
-            case 4: romanNumeral = "IV";
+            case 4:
+                romanNumeral = "IV";
                 break;
-            case 5: romanNumeral = "V";
+            case 5:
+                romanNumeral = "V";
                 break;
-            case 6: romanNumeral = "VI";
+            case 6:
+                romanNumeral = "VI";
                 break;
-            case 7: romanNumeral = "VII";
+            case 7:
+                romanNumeral = "VII";
                 break;
-            case 8: romanNumeral = "VIII";
+            case 8:
+                romanNumeral = "VIII";
                 break;
-            case 9: romanNumeral = "IX";
+            case 9:
+                romanNumeral = "IX";
                 break;
-            case 10: romanNumeral = "X";
+            case 10:
+                romanNumeral = "X";
                 break;
-            default: romanNumeral = "ERROR: Outside the range of 1 through 10";
+            default:
+                romanNumeral = "ERROR: Outside the range of 1 through 10";
                 break;
         }
         System.out.println(romanNumeral);
